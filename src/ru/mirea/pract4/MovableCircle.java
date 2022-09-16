39c007afcfd570c001b4c8e1d8408d14e5dc98d3
Alexandr Pravdin <92106675+AlexandrPravdin@users.noreply.github.com>
2022-09-16 19:13:52 +0300

    pract 4 in progress

---
+++ src/ru/mirea/pract4/MovableCircle.java
@@ -0,0 +1,20 @@
+package ru.mirea.pract4;
+
+public class MovableCircle implements Movable{
+    private int radius;
+    private MovablePoint center;
+
+
+
+
+
+
+    @Override
+    public void moveUp() {}
+    @Override
+    public void moveDown() {}
+    @Override
+    public void moveLeft() {}
+    @Override
+    public void moveRight() {}
+}
---
+++ src/ru/mirea/pract4/MovablePoint.java
@@ -0,0 +1,26 @@
+package ru.mirea.pract4;
+
+public class MovablePoint implements Movable{
+    Package int x;
+    Package int y;
+    Package int xSpeed;
+    Package int ySpeed;
+
+    private MovableCircle movableCircle;
+
+
+
+
+
+
+
+
+    @Override
+    public void moveUp() {}
+    @Override
+    public void moveDown() {}
+    @Override
+    public void moveLeft() {}
+    @Override
+    public void moveRight() {}
+}
