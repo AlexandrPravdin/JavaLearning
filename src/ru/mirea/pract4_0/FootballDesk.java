b416397164a542bc0a84bf2691686bb20c17e8fe
Alexandr Pravdin <92106675+AlexandrPravdin@users.noreply.github.com>
2022-09-18 14:16:47 +0300

    4 pract is full DONE!

--- src/ru/mirea/pract4_0/FootballDesk.java
+++ src/ru/mirea/pract4_0/FootballDesk.java
@@ -7,46 +7,54 @@ import java.awt.event.ActionListener;
 
 public class FootballDesk extends JFrame{
 
-    private JButton milanBttn = new JButton("Milan!!");
+    private JButton milanBttn = new JButton("AC Milan");
 
     private JButton realBttn = new JButton("Real Madrid");
     private int milanNum = 0;
     private int realNum = 0;
     private JButton endGame = new JButton("End Game!");
-    JLabel label = new JLabel(milanNum + "x" + realNum, SwingConstants.CENTER);
+    JLabel label = new JLabel("Result " + milanNum + "x" + realNum, SwingConstants.CENTER);
+    JLabel scoreLabel = new JLabel("Last Scorer: N/A", SwingConstants.CENTER);
 
     public FootballDesk(){
         super("Match");
-        setSize(300, 100);
+        setSize(400, 200);
         add(milanBttn,BorderLayout.EAST);
         add(realBttn,BorderLayout.WEST);
         add(label,BorderLayout.CENTER);
         add(endGame,BorderLayout.SOUTH);
+        add(scoreLabel,BorderLayout.NORTH);
         milanBttn.setPreferredSize(new Dimension(100, 100));
         realBttn.setPreferredSize(new Dimension(100, 100));
         realBttn.addActionListener(new realActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
                 realNum++;
-                label.setText(realNum + "x" + milanNum);
+                label.setText("Result " + realNum + "x" + milanNum);
             }
         });
         milanBttn.addActionListener(new milanActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
                 milanNum++;
-                label.setText(realNum + "x" + milanNum);
+                label.setText("Result " + realNum + "x" + milanNum);
             }
         });
         endGame.addActionListener(new endActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
-                milanBttn.setVisible(false);
-                realBttn.setVisible(false);
+
                 if (realNum < milanNum){
-                label.setText("Milan win!!! with " + realNum + "x" + milanNum);}
-                else
-                {label.setText("Real Madrid win!! with " + realNum + "x" + milanNum );}
+                label.setText("Winner: AC Milan " + realNum + "x" + milanNum);
+                scoreLabel.setText("Last Scorer: Milan");
+                }
+                else {
+                    label.setText("Winner: Real Madrid " + realNum + "x" + milanNum );
+                    scoreLabel.setText("Last Scorer: Madrid");
+                }
+
+                realNum = 0;
+                milanNum = 0;
             }
         });
         setVisible(true);
